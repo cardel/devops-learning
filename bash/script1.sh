0f52c81da7d15280d7ced4684d01402d7147280f
Carlos Delgado <carlos.andres.delgado@correounivalle.edu.co>
2023-09-16 21:06:05 -0500

    16 sep

---
+++ bash/script1.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#
+# This script is used to test the script2.sh script.
+
+# Set the variables
+VAR1="Hello"
+
+echo "This is script 1"
+echo "VAR1 is set to $VAR1"
---
+++ bash/script2.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# This script is used to demonstrate the use of the source command.
+
+echo "Enter a dir name"
+read dirname
+echo "The value of dirname is $dirname"
